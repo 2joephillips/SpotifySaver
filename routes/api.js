f5583ba01a4dd4003599fa7426410e6ad7bff8d3
Joseph Phillips <joseph@josephephillips.com>
2017-01-04 22:33:29 -0500

    first commit :)

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+const URL = 'https://sheets.googleapis.com/v4/spreadsheets/';
+const SpreadSheet = '<SpreadSheetKey>';
+const Values = '/values/Sheet1!A2:F?';
+const API = 'key=<PersonalApi>'
+
+const FULLURL = URL + SpreadSheet + Values + API
+/* GET api listing. */
+router.get('/', (req, res) => {
+    res.send('api works');
+});
+
+/* Get all users. */
+router.get('/songs', (req, res) => {
+    request( FULLURL , 
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(response);
+            console.log(body);
+            res.send(body).status(200)
+        }
+    })
+});
+
+
+module.exports = router;
