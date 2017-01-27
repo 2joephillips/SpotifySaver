06d372f69ca8a874f73c29be5cfd64a17a0b759a
Joseph Phillips <joseph@josephephillips.com>
2017-01-26 22:08:28 -0500

    Add env for keys

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,9 @@ const request = require('request');
 const router = express.Router();
 
 const URL = 'https://sheets.googleapis.com/v4/spreadsheets/';
-const SpreadSheet = '<SpreadSheet>';
-const Values = '/values/Sheet1!A2:F?';
-const API = 'key=<Key>'
+const SpreadSheet = process.env.SPREADSHEET;
+const Values = '/values/Sheet1!A2:F?key=';
+const API = process.env.APIKEY
 
 const FULLURL = URL + SpreadSheet + Values + API
 /* GET api listing. */
